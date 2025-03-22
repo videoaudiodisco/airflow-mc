234d00cfcc64013de7966e9e0c52d2a3ff155afa
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-23 01:28:14 +0900

    op args mod

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,7 +1,7 @@
 def get_sftp():
     print('sftp work start')
 
-def regist(name, sex, **args):
+def regist(name, sex, *args):
     print(f'이름 : {name}')
     print(f'성별: {sex}')
     print(f'기타 옵션 : {args}')
