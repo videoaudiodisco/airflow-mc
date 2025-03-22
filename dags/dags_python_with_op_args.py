c64b82eb669676f2423bd76ca23170b0826ef5d8
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-23 01:06:05 +0900

    op args

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+from common.common_func import regist
+import random
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025,3,1, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id= 'regist_t1',
+        python_callable=regist,
+        op_args=['gypark', 'man', 'kr', 'seoul']
+    )
+    
+    regist_t1
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,7 @@
 def get_sftp():
     print('sftp work start')
+
+def regist(name, sex, **args):
+    print(f'이름 : {name}')
+    print(f'성별: {sex}')
+    print(f'기타 옵션 : {args}')
