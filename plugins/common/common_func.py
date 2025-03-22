e5d142425110ce0b3c22cbcca8851f6c4d96c788
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-23 01:14:23 +0900

    args

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -5,3 +5,15 @@ def regist(name, sex, **args):
     print(f'이름 : {name}')
     print(f'성별: {sex}')
     print(f'기타 옵션 : {args}')
+
+
+def regist2(name, sex, *args, **kwargs):
+    print(f'이름: {name}')
+    print(f'성별: {sex}')
+    print(f'기타옵션들: {args}')
+    email = kwargs.get('email') or None
+    phone = kwargs.get('phone') or None
+    if email:
+        print(email)
+    if phone:
+        print(phone)
