dfe4993597b17d925a3ced4fe1eeddc13e1ed070
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-24 07:36:18 +0900

    mod

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -12,9 +12,8 @@ with DAG(
     start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
     catchup=False,
     # dagrun_timeout=datetime.timedelta(minutes=60),
-
-
 ) as dag:
+    
     bash_t1 = BashOperator(
         task_id='bash_t1',
         bash_command='echo "data_interval_end: {{data_interval_end}}"',
@@ -25,9 +24,8 @@ with DAG(
         env={
             'START_DATE' : '{{data_interval_start | ds}}', # YYYY-MM-DD 형식으로 출력
             'END_DATE' : '{{data_interval_end | ds}}',
-            
         },
-        bash_command='echo &START_DATE && echo $END_DATE',
+        bash_command='echo $START_DATE && echo $END_DATE',
     )
 
     bash_t1 >> bash_t2
