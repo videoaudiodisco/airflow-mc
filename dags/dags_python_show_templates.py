c2f290e9bbd7b3bf7a6cdfbcf000fa5f59f21a31
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-26 22:25:13 +0900

    show

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+import random
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025,3,10, tz='Asia/Seoul'),
+    catchup=True
+) as dag:
+
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+
+        pprint(kwargs)
+
+    show_templates()
