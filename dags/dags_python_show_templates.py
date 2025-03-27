73ee79cd1420903307ad9137304745cb41796922
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-28 03:46:46 +0900

    template error fix

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -2,7 +2,9 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
+from airflow.decorators import task
 import random
+
 
 with DAG(
     dag_id="dags_python_show_templates",
