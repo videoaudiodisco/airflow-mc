eee3a333cac7b2d2211457406915c5c129516bbd
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-28 14:45:56 +0900

    bash macros

---
+++ dags/dags_bash_with_macro_eg1.py
@@ -0,0 +1,30 @@
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1", 
+    schedule="10 0 L * *", # 분 시 일 월 요일
+    start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+) as dag:
+    
+    # START_DATE : 전월 말일, END_DATE : 1일 전
+    bash_task_1 = BashOperator(
+        task_id='bash_task_1',
+        env={
+            'START_DATE' : '{{ data_interval_start.in_time_zone("Asia/Seoul") | ds}}',
+            'END_DATE' : '{{ data_interval_end.in_time_zone("Asia/Seoul") - macros.timedelta(days=-1) | ds}}',
+        },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"',
+    )
+
+
+    bash_task_1 
---
+++ dags/dags_bash_with_macro_eg2.py
@@ -0,0 +1,27 @@
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1", 
+    schedule="10 0 * * 6#2", # 분 시 일 월 요일
+    start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+) as dag:
+    
+    # START_DATE : 2주전 월요일, END_DATE : 2주전 토요일
+    bash_task_2 = BashOperator(
+        task_id='bash_task_2',
+        env={
+            'START_DATE' : '{{}}',
+            'END_DATE' : '{{}}',
+        },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"',
+    )
+
+    bash_task_2 
