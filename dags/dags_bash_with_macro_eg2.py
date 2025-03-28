306b880054d928b2af5567faab5f93b8e79ad587
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-28 17:12:52 +0900

    tmd

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -18,8 +18,8 @@ with DAG(
     bash_task_2 = BashOperator(
         task_id='bash_task_2',
         env={
-            'START_DATE' : '{{ data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19) | ds }}',
-            'END_DATE' : '{{ data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14) | ds }}',
+            'START_DATE' : '{{ (data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds }}',
+            'END_DATE' : '{{ ( data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14) )| ds }}',
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"',
     )
