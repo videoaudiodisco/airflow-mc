49c3b7205febf11b9c66e821cfed19ea911a66d5
videoaudiodisco <gyungyoonpark@gmail.com>
2025-03-28 16:23:10 +0900

    eg2

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -21,7 +21,7 @@ with DAG(
         task_id='bash_task_1',
         env={
             'START_DATE' : '{{ data_interval_start.in_time_zone("Asia/Seoul") | ds}}',
-            'END_DATE' : '{{ data_interval_end.in_time_zone("Asia/Seoul") - macros.timedelta(days=-1) | ds}}',
+            'END_DATE' : '{{ data_interval_end.in_time_zone("Asia/Seoul") - macros.timedelta(days=1) | ds}}',
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"',
     )
--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -7,7 +7,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="dags_bash_with_macro_eg1", 
+    dag_id="dags_bash_with_macro_eg2", 
     schedule="10 0 * * 6#2", # 분 시 일 월 요일
     start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
     catchup=False,
@@ -18,8 +18,8 @@ with DAG(
     bash_task_2 = BashOperator(
         task_id='bash_task_2',
         env={
-            'START_DATE' : '{{}}',
-            'END_DATE' : '{{}}',
+            'START_DATE' : '{{ data_interval_start.in_time_zone("Asia/Seoul") - macros.timedelta(days=19) | ds }}',
+            'END_DATE' : '{{ data_interval_start.in_time_zone("Asia/Seoul") - macros.timedelta(days=14) | ds }}',
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"',
     )
