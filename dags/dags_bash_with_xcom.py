7acb21d6cf47fdb6b9e689d0d250b890f825bfa1
videoaudiodisco <gyungyoonpark@gmail.com>
2025-04-05 11:46:30 +0900

    tmp

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -13,12 +13,21 @@ with DAG(
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
+    
+    # bash_push = BashOperator(
+    # task_id='bash_push',
+    # bash_command="echo START && "
+    #              "echo XCOM_PUSHED "
+    #              "{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} && "
+    #              "echo COMPLETE"
+    # )
+
     bash_push = BashOperator(
     task_id='bash_push',
-    bash_command="echo START && "
-                 "echo XCOM_PUSHED "
-                 "{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} && "
-                 "echo COMPLETE"
+    bash_command="echo start && "
+                "echo xcom_pushed "
+                "{{ ti.xcom_push(key='bash_pushed', value='first_bash_message') }} && "
+                "echo complete"
     )
 
     bash_pull = BashOperator(
@@ -32,20 +41,8 @@ with DAG(
     bash_push >> bash_pull
 
 
-# with DAG(
-#     dag_id="dags_bash_with_xcom", # 화면에서 보이는 값값
-#     schedule="0 0 * * *", # 분 시 일 월 요일
-#     start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
-#     catchup=False,
-# ) as dag:
     
-#     bash_push = BashOperator(
-#         task_id='bash_push',
-#         bash_command="echo start &&"
-#                     "echo xcom_pushed"
-#                     "{{ ti.xcom_push(key='bash_pushed', value='first_bash_message') }} && "
-#                     "echo complete"
-#     )
+
 
 #     bash_pull = BashOperator(
 #         task_id='bash_pull',
