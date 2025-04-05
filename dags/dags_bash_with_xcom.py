8ce610605c8b4db6b82705408796db85f61a1d56
videoaudiodisco <gyungyoonpark@gmail.com>
2025-04-05 11:53:20 +0900

    tmp

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -14,45 +14,23 @@ with DAG(
     catchup=False
 ) as dag:
     
-    # bash_push = BashOperator(
-    # task_id='bash_push',
-    # bash_command="echo START && "
-    #              "echo XCOM_PUSHED "
-    #              "{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} && "
-    #              "echo COMPLETE"
-    # )
-
     bash_push = BashOperator(
     task_id='bash_push',
     bash_command="echo start &&"
                 "echo xcom_pushed"
-                "{{ ti.xcom_push(key='bash_pushed', value='first_bash_message') }} &&"
+                "{{ ti.xcom_push(key='bash_pushed', value='first_bash_message')}} &&"
                 "echo complete"
     )
 
     bash_pull = BashOperator(
         task_id='bash_pull',
-        env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed') }}",
-            'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push') }}"},
-        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE ",
-        do_xcom_push=False
+        env = {
+            'pushed_value' : "{{ ti.xcom_pull(key='bash_pushed') }}",  ## key는 반드시 string 형식으로 입력
+            'return_value' : "{{ ti.xcom_pull(task_ids='bash_push') }}",
+        },
+        bash_command="echo $pushed_value && echo $return_value",
+        do_xcom_push=False,
     )
 
     bash_push >> bash_pull
-
-
-    
-
-
-#     bash_pull = BashOperator(
-#         task_id='bash_pull',
-#         env = {
-#             'pushed_value' : "{{ ti.xcom_pull(key=bash_pushed) }}",
-#             'return_value' : "{{ ti.xcom_pull(task_ids='bash_push') }}",
-#         },
-#         bash_command="echo $pushed_value && echo $return_value",
-#         do_xcom_push=False,
-#     )
-
-#     bash_push >> bash_pull
 
