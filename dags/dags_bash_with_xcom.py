06d0a3c02f899eece22901cda2c137c9f9113a89
videoaudiodisco <gyungyoonpark@gmail.com>
2025-04-05 11:26:53 +0900

    bash xcom

---
+++ dags/dags_bash_with_xcom.py
@@ -0,0 +1,35 @@
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_bash_with_xcom", # 화면에서 보이는 값값
+    schedule="0 0 * * *", # 분 시 일 월 요일
+    start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    bash_push = BashOperator(
+        task_id='bash_push',
+        bash_command="echo start &&"
+        "echo xcom_pushed"
+        "{{ ti.xcom_push(key='bash_pushed', value='first_bash_message')}} &&"
+        "echo complete"
+    )
+
+    bash_pull = BashOperator(
+        task_id='bash_pull',
+        env = {
+            'pushed_value' : "{{ ti.xcom_pull(key=bash_pushed) }}",
+            'return value' : "{{ ti.xcom_pull(task_ids='bash_push') }}",
+        }
+        bash_command="echo $pushed_value && echo $return_value",
+        do_xcom_push=False,
+    )
+
+    bash_push >> bash_pull
+
