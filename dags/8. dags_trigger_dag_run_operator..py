54ad874acfc0534799136f5084eae1711680b4bb
videoaudiodisco <gyungyoonpark@gmail.com>
2025-04-12 22:57:07 +0900

    trigger

--- dags/8. dags_trigger_dag_run_operator..py
+++ dags/8. dags_trigger_dag_run_operator..py
@@ -6,26 +6,45 @@ import pendulum
 
 with DAG(
     dag_id='dags_trigger_dag_run_operator',
-    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2025,4,1, tz='Asia/Seoul'),
     schedule='30 9 * * *',
     catchup=False
 ) as dag:
     
     start_task = BashOperator(
-        task_id = 'start_task',
-        bash_command = 'echo "Start Task"'
+        task_id='start_task',
+        bash_command='echo "start!"',
     )
 
     trigger_dag_task = TriggerDagRunOperator(
-        task_id = 'trigger_dag_task',
-        trigger_dag_id= 'dags_python_operator', # 이 task가 trigger 하는 dag
-        trigger_run_id= None, # None이면 자동으로 run_id가 생성됨 --> manual로 나온다. 
-        execution_date= '{{data_interval_start}}',
-        reset_dag_run=True, # True이면 trigger_dag_id의 dag_run이 reset됨
-        wait_for_completion=False, # True이면 trigger_dag_id의 dag_run이 완료될 때까지 대기함
-        poke_interval=60, 
-        allowed_states=['success'], # trigger_dag_id의 dag_run이 success 상태일 때만 trigger됨
-        failed_states=None,
-    )
+        task_id='trigger_dag_task',
+        trigger_dag_id='dags_python_operator',
+        trigger_run_id=None,
+        execution_date='{{data_interval_start}}',
+        reset_dag_run=True,
+        wait_for_completion=False,
+        poke_interval=60,
+        allowed_states=['success'],
+        failed_states=None
+        )
 
     start_task >> trigger_dag_task
+
+    # start_task = BashOperator(
+    #     task_id = 'start_task',
+    #     bash_command = 'echo "Start Task"'
+    # )
+
+    # trigger_dag_task = TriggerDagRunOperator(
+    #     task_id = 'trigger_dag_task',
+    #     trigger_dag_id= 'dags_python_operator', # 이 task가 trigger 하는 dag
+    #     trigger_run_id= None, # None이면 자동으로 run_id가 생성됨 --> manual로 나온다. 
+    #     execution_date= '{{data_interval_start}}',
+    #     reset_dag_run=True, # True이면 trigger_dag_id의 dag_run이 reset됨
+    #     wait_for_completion=False, # True이면 trigger_dag_id의 dag_run이 완료될 때까지 대기함
+    #     poke_interval=60, 
+    #     allowed_states=['success'], # trigger_dag_id의 dag_run이 success 상태일 때만 trigger됨
+    #     failed_states=None,
+    # )
+
+    # start_task >> trigger_dag_task
