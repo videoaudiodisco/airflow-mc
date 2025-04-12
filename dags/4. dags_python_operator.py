95b4ac46192e2171f7d072cc243a6046d2cd8c42
videoaudiodisco <gyungyoonpark@gmail.com>
2025-04-12 23:15:02 +0900

    tmp

--- dags/4. dags_python_operator.py
+++ dags/4. dags_python_operator.py
@@ -8,7 +8,7 @@ with DAG(
     dag_id="dags_python_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2025,3,1, tz='Asia/Seoul'),
-    catchup=False
+    catchup=True,
 ) as dag:
     
     def select_fruit():
