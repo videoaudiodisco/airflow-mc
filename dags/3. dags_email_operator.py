424f2ae7653b04aa323c154ce0ab93ed2ffa7369
videoaudiodisco <gyungyoonpark@gmail.com>
2025-04-14 06:35:16 +0900

    http

--- dags/3. dags_email_operator.py
+++ dags/3. dags_email_operator.py
@@ -7,8 +7,8 @@ from airflow.operators.email import EmailOperator
 
 with DAG(
     dag_id="dags_email_operator",
-    schedule="0 8 1 * *",
-    start_date=pendulum.datetime(2025,3,1, tz='Asia/Seoul'),
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025,4,1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     
