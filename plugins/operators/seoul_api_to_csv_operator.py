ca17d9d4ca9f76541fa876ce2bd078e1a9d10582
videoaudiodisco <gyungyoonpark@gmail.com>
2025-04-24 23:55:14 +0900

    tmp

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -15,35 +15,35 @@ class SeoulApiToCsvOperator(BaseOperator):
         self.http_conn_id = 'openapi.seoul.go.kr'
         self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/' + dataset_nm
 
-        def execute(self, context):
-            import os
+    def execute(self, context):
+        import os
 
-            # 이전에 ui에서 등록한 variable을 가져옴
-            connection = BaseHook.get_connection(self.http_conn_id)
+        # 이전에 ui에서 등록한 variable을 가져옴
+        connection = BaseHook.get_connection(self.http_conn_id)
 
-            ## 이전에 ui 에서 등록할때 port 까지 같이 host에 등록했기 때문에 수정 필요?
-            # self.base_url = f"http://{connection.host}:{connection.port}/{self.endpoit}"
-            self.base_url = f"http://{connection.host}{self.endpoint}"
+        ## 이전에 ui 에서 등록할때 port 까지 같이 host에 등록했기 때문에 수정 필요?
+        # self.base_url = f"http://{connection.host}:{connection.port}/{self.endpoit}"
+        self.base_url = f"http://{connection.host}{self.endpoint}"
 
-            total_row_df = pd.DataFrame()
-            start_row = 1
-            end_row = 1000
+        total_row_df = pd.DataFrame()
+        start_row = 1
+        end_row = 1000
 
-            # 1000개 씩 가져오되, 마지막 개수가 1000개 미만이면 더이상 데이터 없으므로 중단
-            while True:
-                self.log.info(f'시작 : {start_row}')
-                self.log.info(f'끝 : {end_row}')
-                row_df = self._call_api(self.base_url, start_row, end_row)
-                total_row_df = pd.concat([total_row_df, row_df])
-                if len(row_df) < 1000:
-                    break
-                else:
-                    start_row = end_row + 1
-                    end_row += 1000
+        # 1000개 씩 가져오되, 마지막 개수가 1000개 미만이면 더이상 데이터 없으므로 중단
+        while True:
+            self.log.info(f'시작 : {start_row}')
+            self.log.info(f'끝 : {end_row}')
+            row_df = self._call_api(self.base_url, start_row, end_row)
+            total_row_df = pd.concat([total_row_df, row_df])
+            if len(row_df) < 1000:
+                break
+            else:
+                start_row = end_row + 1
+                end_row += 1000
 
-            if not os.path.exists(self.path):
-                os.system(f'mkdir -p {self.path}')
-            total_row_df.to_csv(f'{self.path}/{self.file_name}', index=False, encoding='utf-8')
+        if not os.path.exists(self.path):
+            os.system(f'mkdir -p {self.path}')
+        total_row_df.to_csv(f'{self.path}/{self.file_name}', index=False, encoding='utf-8')
 
     def _call_api(self, base_url, start_row, end_row):
         import requests
