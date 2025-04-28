7b5684bedd995cbd224587484645b0064d0888ee
videoaudiodisco <gyungyoonpark@gmail.com>
2025-04-28 23:19:42 +0900

    hook

--- dags/9. dags_python_with_postgres.py
+++ dags/9. dags_python_with_postgres.py
@@ -11,7 +11,6 @@ with DAG(
 ) as dag:
 
 
-    
     def insrt_postgres(ip, port, dbname, user, passwd, **kwargs):
         import psycopg2
         from contextlib import closing
@@ -31,5 +30,11 @@ with DAG(
         python_callable=insrt_postgres,
         op_args=["172.28.0.3", "5432", "gypark", "gypark", "gypark"]
     )
+
+    # 중요 : postgres_custom을 5430:5432로 만들었는데, 여기서 5432를 써야 한다.
+    # 이유 : 파이썬 코드가 실행되는 곳이 laptop이나 local 이라면 5430으로 해야 함
+    # 그러나, 만약 같은 network 상의 다른 컨테이너로부터 라면 5432로 해야한다.
+    # airflow는 webserver와 scheduler가 같은 network 상에 있기 때문에 5432로 해야 한다.
+    # 이건 connection hook 만들때도 동일!
         
     insrt_postgres
